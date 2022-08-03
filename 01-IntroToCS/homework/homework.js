9c1166aaf36b2077388aa393c59b03cc42bed346
manuel salvador <salvadorm3578@hotmail.com>
2022-08-03 01:59:59 -0500

    homework 1 ok

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,10 +3,50 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+  
+  var dato=String(num);
+
+  acomulador = 0;
+
+  for (let i=1 ; i <= dato.length; i++) {
+    
+    var areglo = i - 1 ;
+    var exponente = dato.length - i;
+    var calculo = Math.pow(2,exponente) * dato[areglo];
+    var acomulador = acomulador + calculo;
+
+  }
+  return acomulador;
+
+
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+
+  var resul = 0;
+  var dato=num;
+  var areglo=[];
+
+  for (let i=0 ; i < 1;) {
+  var mod=dato%2;
+  var dato=dato/2;
+  dato = Math.floor(dato); 
+  if (dato===1){
+    areglo.unshift(mod);
+    areglo.unshift(dato);
+    resul=areglo.join("");
+    i++; 
+    return resul;
+  }else{
+    areglo.unshift(mod);
+  }
+  
+
+  }
+
+
 
 }
 
